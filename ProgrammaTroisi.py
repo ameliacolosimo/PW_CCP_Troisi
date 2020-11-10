0e278712b90b3f3be271bdb99ddbefc7062c02ec
ameliacolosimo <colosimoamelia@gmail.com>
2020-11-10 12:30:47 +0100

    prove di codice sql in collegamento con php my admin

--- ProgrammaTroisi.py
+++ ProgrammaTroisi.py
@@ -1,7 +1,6 @@
 import os
 import pymysql
 import sqlite3 as spl
-import pymysql.cursors
 import datetime
 conn = pymysql.connect(host='localhost',
                        user='root',
@@ -10,6 +9,23 @@ conn = pymysql.connect(host='localhost',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 cur = conn.cursor()
+mycursor = conn.cursor()
+mycursor.execute("SHOW DATABASES")
+for x in mycursor:
+  print(x)
 
+mycursor = conn.cursor()
+mycursor.execute("SHOW TABLES")
+for x in mycursor:
+  print(x)
 
+mycursor = conn.cursor()
+sql = "INSERT INTO contatori( id_contatore, id_utente, quantità_totale) VALUES (%s, %s, %s)"
+
+val = [
+    (1, 1, 3678978),
+    (2, 2, 3432566435)
+]
+mycursor.executemany(sql,val)
+conn.commit()
 
