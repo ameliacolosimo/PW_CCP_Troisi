cb847c347e671de14b5990417dea278867de1d80
ameliacolosimo <colosimoamelia@gmail.com>
2020-11-10 19:34:54 +0100

    utilizzo lista per inserimento tabelle

--- demo.py
+++ demo.py
@@ -1,9 +1,3 @@
-print('hello')
-print('Welcome')
-print('FabiocuoreNicola')
-print('Luca<3Fabio<3Nicola')
-print('gli indiani ci salveranno, lo sento!')
-print ('spiega proprio bene su IUTUBBE Alkhmir')
 
 
 
