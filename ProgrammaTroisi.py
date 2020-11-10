63d6712e816d1cd064c0a9b568f05516cc7b1d01
ameliacolosimo <colosimoamelia@gmail.com>
2020-11-10 18:51:51 +0100

    prova funzione def

--- ProgrammaTroisi.py
+++ ProgrammaTroisi.py
@@ -24,9 +24,27 @@ mycursor = conn.cursor()
 sql = "INSERT INTO contatori( id_contatore, id_utente, quantità_totale) VALUES (%s, %s, %s)"
 
 val = [
-    (1, 1, 3678978),
-    (2, 2, 3432566435)
-]
-mycursor.executemany(sql,val)
+    (5, 1, 3678978),
+    (6, 2, 3432566435)]
+
 conn.commit()
 
+sql = "INSERT INTO bollette_emesse (id_bolletta_emessa, id_utente, quantita, " \
+      "id_contratto, tariffa, importo, periodo_riferimento, data_emissione, data_scadenza) " \
+      "VALUES (%s, %s, %s, %s,%s, %s, %s, %s, %s)"
+
+val = [
+    (1,1,3516, 1, 68, 0,  "Maggio-Luglio", datetime.date (2020,07,10), detatime.date  (2020,07,25),
+    (2, 2, 3546, 2, 34, 0, "Gennaio-Marzo", datetime.date (2020,03,02), datetime.date  (2020,03,17)]
+
+ALIQUOTA_IVA = 0.22
+COSTO_UNITARIO = 0.20
+COSTO_UNITARIO_SOCIALE = 0.10
+SOGLIA_CONSUMO_SOCIALE = 50.0
+
+def calcola_totale(quantità):
+    importo = 0.0;
+    if quantità <= SOGLIA_CONSUMO_SOCIALE:
+        importo = quantità * COSTO_UNITARIO_SOCIALE
+    else:
+        importo = quantità * COSTO_UNITARIO
